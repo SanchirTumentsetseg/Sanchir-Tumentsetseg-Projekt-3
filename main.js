a3d4a6019a41c84e31359ac2e9c78df226adf4d7
Sanchir Tumentsetseg <tumentsetseg.sanchir@elev.ga.ntig.se>
2022-05-11 22:35:13 +0200

    enter

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ window.addEventListener("scroll", function()
     let offset =window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.007 + "px";
 })
+
+const parallax3 = document.getElementById("parallax3");
+
+window.addEventListener("scroll", function()
+{
+    let offset =window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.007 + "px";
+})
+
+const parallax4 = document.getElementById("parallax4");
+
+window.addEventListener("scroll", function()
+{
+    let offset =window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.007 + "px";
+})
